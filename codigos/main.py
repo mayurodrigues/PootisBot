1ad8a7cd8d0b8fc13df8e6c82e070e948a9f3986
Rosemizr <otapatilera@gmail.com>
2025-01-28 21:03:29 -0300

    Update main.py

--- codigos/main.py
+++ codigos/main.py
@@ -8,16 +8,15 @@ CHANNEL_ID = 1333909539820666934
 permissoes = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents = permissoes)
 
-@bot.command()
-async def ola(ctx:commands.Context):
-    usuario = ctx.author
-    await ctx.reply(f"Falaaaa assombrados!{usuario.name}")
-
 @bot.event
 async def on_ready():
     print("Estou online!")
     channel = bot.get_channel(CHANNEL_ID)
     await channel.send("Fala gatinhas! Estou na área!")
+
+@bot.command()
+async def ola(ctx):
+    await ctx.send("Falaaaa assombrados!")
 
 load_dotenv()
 bot.run(os.getenv('TOKEN'))
