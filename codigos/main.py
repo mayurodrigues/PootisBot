b7ff94174fa1e8153232732d96bf94cce151893a
Mayu <mayurccontato@gmail.com>
2025-01-28 22:20:10 -0300

    Refizemos a estrutura do código do bot utilizando um Client

--- codigos/main.py
+++ codigos/main.py
@@ -3,21 +3,35 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-CHANNEL_ID = 1333909539820666934
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Iniciado como {self.user}!')
 
+    async def on_message(self, message):
+         # Ignorar mensagens do próprio bot
+        if message.author == self.user:
+            return
+
+        print(f'Mensagem de {message.author}: {message.content}')
+        if message.content == '!ola':
+            await message.channel.send(f'Fala, {message.author.name}!')
+
+
+# Na variável "permissões" são colocadas as permissões, que precisam ser ativadas como acontece abaixo
 permissoes = discord.Intents.default()
 permissoes.messages = True
-bot = commands.Bot(command_prefix="!", intents=permissoes)
+permissoes.message_content = True
 
-@bot.event
-async def on_ready():
-    print("Estou online!")
-    channel = bot.get_channel(CHANNEL_ID)
-    await channel.send("Fala gatinhas! Estou na área!")
+# Criando um client
+client = MyClient(intents=permissoes)
 
-@bot.command()
-async def ola(ctx):
-    await ctx.send("Falaaaa assombrados!")
+#
+# @client.command(aliases=['p', 'q'])
+# async def ping(ctx, arg=None):
+#     if arg == "pong":
+#         await ctx.send('Nice job, you just ponged yourself')
+#     else:
+#         await ctx.send(f'Pong! Here is your ping: {round(client.latency * 1000)}ms')
 
 load_dotenv()
-bot.run(os.getenv('TOKEN'))
+client.run(os.getenv('TOKEN'))
