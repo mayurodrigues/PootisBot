f8d1e448130b945cc93603d9eb03f6ad473e1392
Mayu <mayurccontato@gmail.com>
2025-01-28 22:33:02 -0300

    Adicionamos comandos de !ping e !pong

--- codigos/main.py
+++ codigos/main.py
@@ -1,6 +1,5 @@
 import os
 import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 
 class MyClient(discord.Client):
@@ -16,6 +15,11 @@ class MyClient(discord.Client):
         if message.content == '!ola':
             await message.channel.send(f'Fala, {message.author.name}!')
 
+        elif message.content == '!ping':
+            await message.channel.send(f'Pong! Aqui está seu ping: {round(client.latency * 1000)}ms.')
+
+        elif message.content == '!pong':
+            await message.channel.send('Escreveu errado, minha gatinha!')
 
 # Na variável "permissões" são colocadas as permissões, que precisam ser ativadas como acontece abaixo
 permissoes = discord.Intents.default()
@@ -24,14 +28,6 @@ permissoes.message_content = True
 
 # Criando um client
 client = MyClient(intents=permissoes)
-
-#
-# @client.command(aliases=['p', 'q'])
-# async def ping(ctx, arg=None):
-#     if arg == "pong":
-#         await ctx.send('Nice job, you just ponged yourself')
-#     else:
-#         await ctx.send(f'Pong! Here is your ping: {round(client.latency * 1000)}ms')
 
 load_dotenv()
 client.run(os.getenv('TOKEN'))
