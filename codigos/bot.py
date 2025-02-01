0c77041cd4ce64f42cc2faac9bd42257e4e90568
Mayu <mayurccontato@gmail.com>
2025-02-01 10:45:02 -0300

    Update bot.py

--- codigos/bot.py
+++ codigos/bot.py
@@ -3,7 +3,6 @@ import logging
 from discord.ext import commands
 from erros import MENSAGENS_DE_ERRO  # Importa o dicionário de erros no arquivo separado
 from simples import comandos_simples  # Importa comandos simples
-from musica import comandos_musica  # Importa comandos de música
 
 # Configura o logging de erros
 logging.basicConfig(filename='bot.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -18,13 +17,10 @@ INTENTS.guilds = True
 # Cria uma instância do bot
 bot = commands.Bot(command_prefix='!', intents=INTENTS)
 
+# Evento: mensagem no terminal quando o bot inicia
 @bot.event
 async def on_ready():
-    """
-    Evento disparado quando o bot está pronto e conectado ao Discord.
-    """
     comandos_desativados = []  # Inserir nome do comando entre aspas e sem prefixo
-
     for comando in comandos_desativados:
         comando = bot.get_command(comando)
         if comando:
@@ -34,36 +30,29 @@ async def on_ready():
     for guild in bot.guilds:
         print(f'Iniciado como {bot.user} no servidor {guild.name}!')
 
+# Evento: exibe mensagens de chat no terminal
 @bot.event
 async def on_message(message):
-    """
-    Evento disparado quando uma mensagem é enviada no servidor.
-    """
     print(f'Mensagem de {message.author}: {message.content}')
-
     # Garante que o bot não responda a si
     if message.author == bot.user:
         return
-
     # Processa os comandos a partir das mensagens
     await bot.process_commands(message)
 
+# Tratamento de erros
 @bot.event
 async def on_command_error(ctx, error):
-    """
-    Evento disparado quando ocorre um erro em um comando.
-    """
     tipo_de_erro = type(error)
     if tipo_de_erro in MENSAGENS_DE_ERRO:
-        message = MENSAGENS_DE_ERRO[tipo_de_erro]
-        if callable(message):
-            message = message(error)
-        await ctx.reply(message.format(ctx=ctx))
+        mensagem = MENSAGENS_DE_ERRO[tipo_de_erro]
+        if callable(mensagem):
+            mensagem = mensagem(error)
+        await ctx.reply(mensagem.format(ctx=ctx))
     else:
         await ctx.reply('Ocorreu um erro inesperado! Por favor, tente novamente.')
         print(error)
         logging.error(f'{error}', exc_info=True)
 
-# Configura comandos simples e de música
+# Configura os comandos
 comandos_simples(bot)
-comandos_musica(bot)
