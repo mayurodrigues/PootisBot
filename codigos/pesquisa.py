38d79e4528cf056bce88f312e4d61c17b4244737
Mayu <mayurccontato@gmail.com>
2025-02-03 11:49:45 -0300

    Reparo na função de pesquisa e atualização no embed
    Agora não há problemas de conexão e o bot coloca a primeira imagem de uma pesquisa como thumb

--- codigos/pesquisa.py
+++ codigos/pesquisa.py
@@ -5,26 +5,31 @@ from googleapiclient.discovery import build
 
 load_dotenv()
 
-API = os.getenv('GOOGLE_API')
-ID_BASICO = os.getenv('BASICO')
+API_GOOGLE = os.getenv('API_GOOGLE')
+ID_SIMPLES = os.getenv('ID_SIMPLES')
+SIMPLES = build('customsearch', 'v1', developerKey=API_GOOGLE)
 
 def comandos_pesquisa(bot):
     @bot.command()
-    async def pesquisar(ctx, termo: str):
-        logo = disnake.File('material/google_logo.png', filename='google_logo.png')
+    async def pesquisar(ctx, *args):
+        termo = ' '.join(args)
+        LOGO = disnake.File('material/google_logo.png', filename='google_logo.png')
+        embed = disnake.Embed(title=f'Resultados para “{termo}”', colour=0x4285F4)
+        embed.set_footer(text=f'Google Search', icon_url="attachment://google_logo.png")
 
-        embed = disnake.Embed(title=f'Resultados para: {termo}', colour=0x4285F4)
-        embed.set_thumbnail(file=logo)
-        embed.set_author(name="Google Search", icon_file=logo)
-        embed.timestamp = ctx.message.created_at
+        pesquisa = SIMPLES.cse().list(q=termo, cx=ID_SIMPLES, num=3, hl='pt-BR', gl='br', searchType='image').execute()
 
-        basico = build('customsearch', 'v1', developerKey=API)
-        pesquisa = basico.cse().list(q=termo, cx=ID_BASICO, num=3, hl='pt-BR', gl='br').execute()
+        if 'items' in pesquisa and pesquisa['items']:
+            imagem = pesquisa['items'][0].get('link')
+        else: imagem = None
 
+        pesquisa = SIMPLES.cse().list(q=termo, cx=ID_SIMPLES, num=3, hl='pt-BR', gl='br').execute()
         for numero, resultado in enumerate(pesquisa.get('items', [])):
             titulo = resultado.get('title', 'Sem título')
             link = resultado.get('link', 'Sem link')
             descricao = resultado.get('snippet', 'Sem descrição')
-            embed.add_field(name=f'{titulo}', value=f'{descricao}\n[Link]({link})', inline=False)
 
-        await ctx.send(embed=embed)
+            embed.add_field(name=f'**{numero + 1}. {titulo}**', value=f'{descricao}\n[Acessar link]({link})',inline=False)
+
+        if imagem: embed.set_thumbnail(url=imagem)
+        await ctx.send(embed=embed, file=LOGO)
