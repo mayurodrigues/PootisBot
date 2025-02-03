9ff2a8ba3379203c6bbfcac669e1d89a8072066a
Mayu <mayurccontato@gmail.com>
2025-02-03 12:02:34 -0300

    Adição de comentários no pesquisa.py

--- codigos/pesquisa.py
+++ codigos/pesquisa.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 API_GOOGLE = os.getenv('API_GOOGLE')
 ID_SIMPLES = os.getenv('ID_SIMPLES')
-SIMPLES = build('customsearch', 'v1', developerKey=API_GOOGLE)
+SIMPLES = build('customsearch', 'v1', developerKey=API_GOOGLE) # Constroi um mecanismo de busca
 
 def comandos_pesquisa(bot):
     @bot.command()
     async def pesquisar(ctx, *args):
-        termo = ' '.join(args)
-        LOGO = disnake.File('material/google_logo.png', filename='google_logo.png')
+        termo = ' '.join(args) # Define tudo escrito após o comando como parte do termo a ser pesquisado
+        LOGO = disnake.File('material/google_logo.png', filename='google_logo.png') # Variável de arquivo para a logo na pasta (material)
         embed = disnake.Embed(title=f'Resultados para “{termo}”', colour=0x4285F4)
-        embed.set_footer(text=f'Google Search', icon_url="attachment://google_logo.png")
+        embed.set_footer(text=f'Google Search', icon_file=LOGO) # Configura o rodapé
 
         pesquisa = SIMPLES.cse().list(q=termo, cx=ID_SIMPLES, num=3, hl='pt-BR', gl='br', searchType='image').execute()
 
-        if 'items' in pesquisa and pesquisa['items']:
+        if 'items' in pesquisa and pesquisa['items']: # Pega a imagem presente no primeiro resultado, se houver
             imagem = pesquisa['items'][0].get('link')
         else: imagem = None
 
@@ -31,5 +31,5 @@ def comandos_pesquisa(bot):
 
             embed.add_field(name=f'**{numero + 1}. {titulo}**', value=f'{descricao}\n[Acessar link]({link})',inline=False)
 
-        if imagem: embed.set_thumbnail(url=imagem)
+        if imagem: embed.set_thumbnail(url=imagem) # Adiciona a imagem extraída do primeiro resultado como thumb
         await ctx.send(embed=embed, file=LOGO)
