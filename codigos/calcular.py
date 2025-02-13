7c17450bb78c8d7952c9e3a7a983a2a24495e494
Mayu <mayurccontato@gmail.com>
2025-02-13 07:12:36 -0300

    Update calcular.py

--- codigos/calcular.py
+++ codigos/calcular.py
@@ -24,7 +24,7 @@ def comandos_calcular(bot):
                 await ctx.reply(f'Resultado com decimal: {resultado.evalf()}\n'
                                 f'Resultado com radical: {resultado}')
         elif operacao.lower() == 'o amor de mayu pela rosa':
-            await ctx.reply('É imensurável...')
+            await ctx.reply('É imensurável…')
         else:
             await ctx.reply('Essa não é uma operação válida!')
 
